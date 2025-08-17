3b91a8ecbcf1d723dd0e91d0d5088e115f6cb5fb
Bryson Ward <77869922+brysonbw@users.noreply.github.com>
2025-08-17 11:13:53 -0600

    Update build declaration file (#4)

--- src/typings.ts
+++ src/typings.ts
@@ -1,5 +1,4 @@
 // Exporting all to generate one combined declaration file with bundler
-export * from './toast-emitter.ts';
+export { toast } from './toast-emitter.ts';
 export * from './toaster-element.ts';
 export * from './types.ts';
-export * from './utils.ts';
