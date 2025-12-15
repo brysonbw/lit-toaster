f2ff14c785853094cf774eff6bcf3136699ac9a6
brysonbw <77869922+brysonbw@users.noreply.github.com>
2025-12-15 03:56:20 +0000

    chore(release): v0.2.5

--- dist/lit-toaster.js
+++ dist/lit-toaster.js
@@ -1,4 +1,4 @@
-/*! lit-toaster v0.2.4 Copyright (c) 2025 Bryson Ward and contributors MIT License*/
+/*! lit-toaster v0.2.5 Copyright (c) 2025 Bryson Ward and contributors MIT License*/
 import { css, LitElement, html } from 'lit';
 import { property, state, customElement } from 'lit/decorators.js';
 
