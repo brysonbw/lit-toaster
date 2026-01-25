ffcab76453892caf6da2cc455afaa508c8f731fc
Bryson Ward <77869922+brysonbw@users.noreply.github.com>
2026-01-24 18:59:39 -0700

    fix(toaster-element): prevent "global is not defined" error in Rollup by treating file as a module (#50)

--- src/typings.ts
+++ src/typings.ts
@@ -1,4 +1,4 @@
 // Exporting all to generate one combined declaration file with bundler
 export { toast } from './toast-emitter.ts';
-export * from './toaster-element.ts';
+export { ToasterElement } from './toaster-element.ts';
 export * from './types.ts';
